018bbc8003603fb639b38d91fe80c2bd2eeb3042
maechabin <mitoppo@gmail.com>
2019-02-06 01:59:52 +0900

    implement props.children

--- projects/core/src/shared/factory.ts
+++ projects/core/src/shared/factory.ts
@@ -3,7 +3,6 @@ import './jsx'
 import { ChildDef, ClassComponentType, ElementDef, FunctionComponentType } from './types'
 
 export function createElement<P = any>(type: ClassComponentType<P> | FunctionComponentType<P> | Type<any> | string, props?: P, ...children: ChildDef[]): ElementDef<P> {
-  return { type, props: props || null, children }
   return {
     type,
     props: Object.assign({}, props, { c: children }) || null,
