fc9065663d42fdbc9ab4c11d45514c6e67d1d2b3
maechabin <mitoppo@gmail.com>
2019-02-09 21:32:41 +0900

    feat (core): support props.children

--- projects/core/src/instructions/class-component.ts
+++ projects/core/src/instructions/class-component.ts
@@ -8,10 +8,14 @@ import { unmount } from './unmount'
 export function mountClassComponent(kit: RenderKit, vNode: VNode, container: Element | null, nextNode: Node | null): void {
   const type = vNode.type as ClassComponentType
   const props = vNode.props as Properties | null
-  const meta = vNode.meta = createEmptyMeta()
+  const children = (props && props.c) || null
+  const meta = (vNode.meta = createEmptyMeta())
 
-  const instance = meta[COMPONENT_INSTANCE] = new type(props)
-  const inner = meta[RENDER_RESULT] = normalize(instance.render())
+  const instance = (meta[COMPONENT_INSTANCE] = new type({
+    ...props,
+    children,
+  }))
+  const inner = (meta[RENDER_RESULT] = normalize(instance.render()))
 
   mount(kit, inner, container, nextNode)
   vNode.native = inner.native
@@ -20,15 +24,19 @@ export function mountClassComponent(kit: RenderKit, vNode: VNode, container: Ele
 }
 
 export function patchClassComponent(kit: RenderKit, lastVNode: VNode, nextVNode: VNode): void {
-  const meta = nextVNode.meta = lastVNode.meta!
+  const meta = (nextVNode.meta = lastVNode.meta!)
 
   const instance = meta[COMPONENT_INSTANCE]!
   const lastResult = meta[RENDER_RESULT]!
 
   const props = nextVNode.props as Properties
+  const children = (props && props.c) || null
 
-  (instance as { props: Properties }).props = props
-  const nextResult = meta[RENDER_RESULT] = normalize(instance!.render())
+  ;(instance as { props: Properties }).props = {
+    ...props,
+    children,
+  }
+  const nextResult = (meta[RENDER_RESULT] = normalize(instance!.render()))
 
   patch(kit, lastResult, nextResult)
   nextVNode.native = nextResult.native
